5543aaf82416be2fa89c1f8689a556a35e517f24
DESKTOP-MB5UL1P\BOYSB <de.quintanag@gmail.com>
2017-12-18 17:54:31 -0300

    cambio en la galeria.php

--- galeria1.php
+++ galeria1.php
@@ -22,40 +22,22 @@
     <section class="mainContent full-width clearfix homeGallerySection">
       <div class="container">
         <div class="row">
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="card">
-                <img class="card-img-top" src="img/equipo_mem2.jpeg" alt="image">
-                    <div class="card-body">
-                        <h4 class="card-title">Programa Religión</h4>
-                            <p class="card-text">Actividad de Cierre de las redes de Religión del MEM Distrital - Norte</p>
-                            <a href="galeria.php" class="btn btn-primary">Ver Más</a>
-                    </div>
-                </div>
+          <div class="col-sm-4 col-xs-12 block">
+            <div class="thumbnail thumbnailContent">
+              <a href="single-blog-left-sidebar.html"><img src="img/CIERRE2017/2.JPG" alt="image" class="img-responsive"></a>
+              <div class="sticker-round bg-color-3">14 <br>Diciembre</div>
+              <div class="caption border-color-1">
+                <h3><a href="galeria.php" class="color-1">Cena programa de religión</a></h3>
+                
+                <p>Actividad de Cierre de año del programa de religión del MEM Distrito Norte. </p>
+                <ul class="list-inline btn-yellow">
+                  <li><a href="galeria.php" class="btn btn-link"><i class="fa fa-angle-double-right" aria-hidden="true"></i> Ver Más</a></li>
+                </ul>
+              </div>
             </div>
-            
-            <div class="col-md-4 col-sm-6 col-xs-12">
-                <div class="card">
-                <img class="card-img-top" src="img/equipo_mem2.jpeg" alt="image">
-                    <div class="card-body">
-                        <h4 class="card-title">Programa Religión</h4>
-                            <p class="card-text">Actividad de Cierre de las redes de Religión del MEM Distrital - Norte</p>
-                            <a href="galeria.php" class="btn btn-primary">Ver Más</a>
-                    </div>
-                </div>
-            </div>
-            
-                <div class="col-md-4 col-sm-6 col-xs-12">    
-                    <div class="card">
-                        <img class="card-img-top" src="img/equipo_mem2.jpeg" alt="image">
-                            <div class="card-body">
-                                <h4 class="card-title">Programa Religión</h4>
-                                <p class="card-text">Actividad de Cierre de las redes de Religión del MEM Distrital - Norte</p>
-                                <a href="galeria.php" class="btn btn-primary">Ver Más</a>
-                            </div>
-                    </div>
-                </div>
           </div>
         </div>
+    </div>
 </section>
  
 
