cd44db62699c223c283db6680ee73b49e10579d7
Israel Teran <israeliqq@live.cl>
2017-12-23 12:14:01 -0300

    LISTAR POSTULACIONES

--- postular.php
+++ postular.php
@@ -2,7 +2,7 @@
 <?php include("cabezera.php");  
 
 $array = postular();
-$dato = $array->fetch_assoc();
+
 
 ?>
 <!-- HEADER -->
@@ -25,117 +25,41 @@ $dato = $array->fetch_assoc();
 
     <!-- MAIN SECTION -->
     <section class="mainContent full-width clearfix conactSection">
-    <?php for ($var = 1 ; $var <= sizeof ($dato) ; $var++) {?>
-      <div class="container">
-       <div class="row">
-            
-           <div class="col-md-12 col-sm-12 col-xs-12">
-               
-           <table class="table" >
-               
-               <thead>
+
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="cartInfo">
+              <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr class="bg-color-1">
+                      <th scope="col">NOMBRE</th>
+                      <th scope="col">CIUDAD</th>
+                      <th scope="col">CORREO</th>
+                      <th scope="col">TELÉFONO</th>
+                  </tr>
+                </thead>
+                <tbody>
+                <?php while($dato = $array->fetch_assoc()){?>
                 <tr>
-                    <th scope="col">Nombre</th>
-                    <th scope="col">Ciudad</th>
-                    <th scope="col">Correo</th>
-                    <th scope="col">Número de Contacto</th>
-                </tr>
-               </thead>
-            <tbody>
-                    
+
                     <td><?php echo utf8_encode($dato['nombre']); ?></td>
                     <td><?php echo utf8_encode($dato['ciudad']); ?></td>
                     <td><?php echo utf8_encode($dato['email']); ?></td>
                     <td>+56<?php echo utf8_encode($dato['telefono']); ?></td>
-                
-            </tbody>
-           </table>
+
+                </tr>
+                <?php }?>
+                </tbody>
+             </table>
            </div>
-          <!--<div class="col-sm-4 col-xs-12">
-            <div class="media addressContent">
-              <span class="media-left bg-color-4" href="#">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-              </span>
-              <div class="media-body">
-                <h3 class="media-heading">Dirección:</h3>
-                <p>Calle Obispo Labbé #1545</p>
-                <p>Iquique, Región de Tarapacá - Chile</p>
-              </div>
-            </div>
-          </div>-->
-        <!-- <div class="col-sm-4 col-xs-12">
-            <div class="media addressContent">
-              <span class="media-left bg-color-4" href="#">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-              </span>
-              <div class="media-body">
-                <h3 class="media-heading">Teléfono:</h3>
-                <p>+56 9 1234 5678</p>
-                <p>+56 2 222 222</p>
-              </div>
-            </div>
-          </div>-->
-          <!--<div class="col-sm-4 col-xs-12">
-            <div class="media addressContent">
-              <span class="media-left bg-color-4" href="#">
-                <i class="fa fa-envelope-o" aria-hidden="true"></i>
-              </span>
-              <div class="media-body">
-                <h3 class="media-heading">Correo:</h3>
-                <p><a href="mailto:contacto@mem.cl">contacto@mem.cl</a></p>
-                <p><a href="mailto:director@mem.cl">director@mem.cl</a></p>
-              </div>
-            </div>
-          </div>-->
-           <?php }?>
-        </div>
-       
-        <!--<div class="row">
-          <div class="col-xs-12">
-            <div class="homeContactContent">
-              <form id="form1" action="enviar.php" method="post" name="form1" enctype="multipart/form-data">
-                <div class="row">
-                  <div class="col-sm-6 col-xs-12">
-                    <div class="form-group">
-                      <i class="fa fa-user"></i>
-                      <input type="text" class="form-control border-color-1" id="exampleInputEmail1" placeholder="Nombre" name="nombre" required>
-                    </div>
-                  </div>
-                  <div class="col-sm-6 col-xs-12">
-                    <div class="form-group">
-                      <i class="fa fa-envelope" aria-hidden="true"></i>
-                      <input type="text" class="form-control border-color-2" id="exampleInputEmail2" name="correo" placeholder="Correo Electrónico" required>
-                    </div>
-                  </div>
-                  <div class="col-sm-6 col-xs-12">
-                    <div class="form-group">
-                      <i class="fa fa-phone" aria-hidden="true"></i>
-                      <input type="text" class="form-control border-color-5" id="exampleInputEmail3" name="telefono" placeholder="Teléfono" required>
-                    </div>
-                  </div>
-                  <div class="col-sm-6 col-xs-12">
-                    <div class="form-group">
-                      <i class="fa fa-book" aria-hidden="true"></i>
-                      <input type="text" class="form-control border-color-6" id="exampleInputEmail4" name="asunto" placeholder="Asunto" required>
-                    </div>
-                  </div>
-                  <div class="col-xs-12">
-                    <div class="form-group">
-                      <i class="fa fa-comments" aria-hidden="true"></i>
-                      <textarea class="form-control border-color-4" placeholder="Escribir Mensaje" name="mensaje"></textarea>
-                    </div>
-                  </div>
-                  <div class="col-xs-12">
-                    <div class="formBtnArea">
-                      <button type="submit" class="btn btn-primary">Enviar </button>
-                    </div>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>-->
-      </div>
+         </div>
+    </div>
+  </div>
+</div>
     </section>
 
 
