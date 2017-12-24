ee605f12003fe1357e5b1e75f978bdff10bab026
Israel Teran <israeliqq@live.cl>
2017-12-24 13:43:43 -0300

    Arreglo Logo MEM

--- cabezera.php
+++ cabezera.php
@@ -97,13 +97,14 @@
         <div class="container">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
+            <img src="img/logomem3.png" class="navbar-brand" style="width:300px;">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php"><img src="img/logomem.png" alt="logo"> </a>
+
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
